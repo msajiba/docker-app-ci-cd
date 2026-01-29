54de9a57b4bd44b016014548a6e37d9ec4ae5eea
msajiba <asajib499@gmail.com>
2026-01-30 00:34:29 +0600

    solved the issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Docker App again</h1>
-      <button>hello</button>
+      <h1> tag</h1>
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+        ignored: /node_modules/,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
