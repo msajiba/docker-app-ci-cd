b9e5b6ff6fdca389504b199ea8410aaa2d55a5e8
msajiba <asajib499@gmail.com>
2026-01-30 00:42:46 +0600

    Update Dockerfile for development environment and modify Home component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Docker App again</h1>
+      <h1>Welcome to Docker App a</h1>
+      <p>daa</p>
       <button>hello</button>
     </div>
   );
